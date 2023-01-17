194322a951a6a2411b7c42831aae2d0360905447
RAFALAMAO <aaronjt@outlook.com>
2023-01-17 17:54:04 -0600

    Added simple style admin dash

--- src/components/AdminDash/AdminDash.jsx
+++ src/components/AdminDash/AdminDash.jsx
@@ -33,6 +33,25 @@ export default function AdminDash() {
     }
   }
 
+  const urlControll = (user) => {
+    if( user.user_doctos && user.user_doctos.length > 0 )
+      return <a
+        href={`${user.user_doctos[0].url}`}
+        target={'_blank'}
+        rel="noreferrer"
+      >Artículo</a>
+    else {
+      return `---`;
+    }
+  }
+
+  const existOrVoidStr = (user) => {
+    if( user.application && user.application.gustarias_laborar )
+      return `${user.application.gustarias_laborar}`;
+    else
+      return `---`;
+  }
+
   // =========== UseEffects
   useEffect(() => {
     fetchData();
@@ -43,12 +62,27 @@ export default function AdminDash() {
     <>
       <Nav/>
       <div className='admin-dash'>
-        <h1>AdminDash</h1>
-        {usersDoctosAndInfo.map((user, idx) => {
-          return <div key={idx}>
-            <p>{`${user.primer_nombre} ${user.apellido_paterno}`}</p>
+        <h1>Bienvenido administrador de BBVA Talent</h1>
+        <p>En la siguiente tabla podras ver los enlaces de cada uno de los articulos registrados, así como información relacionada a cada uno de ellos.</p>
+        <div className='admin-dash-table-container'>
+          {/* Header de tabla*/}
+          <div className='admin-table-titles'>
+            <p>Nombre</p>
+            <p>Correo</p>
+            <p>Título artículo</p>
+            <p>Gustaria laborar</p>
+            <p>Enlace</p>
           </div>
-        })}
+          {usersDoctosAndInfo.map((user, idx) => {
+            return <div className='admin-table-data' key={idx}>
+              <p>{`${user.primer_nombre} ${user.apellido_paterno}`}</p>
+              <p>{`${user.correo_electronico}`}</p>
+              <p>{`${user.application.nombre_articulo}`}</p>
+              <p>{existOrVoidStr(user)}</p>
+              {urlControll(user)}
+            </div>
+          })}
+        </div>
       </div>
       <Footer/>
     </>
